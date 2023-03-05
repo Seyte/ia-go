a2e7c334fc2cf9210d620701b11a964547b0eec4
LucasSenis <lucas@senis.org>
2023-03-05 23:07:54 +0100

    Ajout de deux heuristiques, une basique et une plus complexe

--- go-package/myPlayer.py
+++ go-package/myPlayer.py
@@ -22,7 +22,45 @@ class myPlayer(PlayerInterface):
         self._mycolor = None
 
     def getPlayerName(self):
-        return "Random Player"
+        return "'Not random anymore' Player"
+
+    def heuristique_simple(self):
+        assert(self._mycolor != None)
+        result = self._board.diff_stones_board() + 2*self._board.diff_stones_captured()
+        if (self._mycolor == Goban.Board._BLACK):
+            return result
+        elif (self._mycolor == Goban.Board._WHITE):
+            return -result
+        return 0
+    
+    def heuristique_complexe(self):
+        assert(self._mycolor != None)
+        array = self._board.get_board()
+        holes_in_black = 0
+        holes_in_white = 0
+        print(array)
+        array[1] = Goban.Board._BLACK
+        array[9] = Goban.Board._BLACK
+        print(array)
+        for i in range(9):
+            for j in range(9):
+                if (array[i+9*j] == Goban.Board._EMPTY):
+                    if (((i == 0) or (array[(i-1)+9*j] == Goban.Board._BLACK))
+                        and ((i == 8) or (array[(i+1)+9*j] == Goban.Board._BLACK))
+                        and ((j == 0) or (array[i+9*(j-1)] == Goban.Board._BLACK))
+                        and ((j == 8) or (array[i+9*(j+1)] == Goban.Board._BLACK))):
+                        holes_in_black += 1
+                    elif (((i == 0) or (array[(i-1)+9*j] == Goban.Board._WHITE))
+                        and ((i == 8) or (array[(i+1)+9*j] == Goban.Board._WHITE))
+                        and ((j == 0) or (array[i+9*(j-1)] == Goban.Board._WHITE))
+                        and ((j == 8) or (array[i+9*(j+1)] == Goban.Board._WHITE))):
+                        holes_in_white += 1
+        result = holes_in_black - holes_in_white
+        if (self._mycolor == Goban.Board._BLACK):
+            return 3*result + heuristique_simple()
+        elif (self._mycolor == Goban.Board._WHITE):
+            return -3*result + heuristique_simple()
+        return 0
 
     def getPlayerMove(self):
         if self._board.is_game_over():
